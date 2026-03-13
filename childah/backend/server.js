f1a0096e56dac88f32ff6dbdd70a0fa358e46333
childah.Al.jeune <childah.jeune@escola.pr.gov.br>
2026-03-13 10:52:41 -0300

    server

--- childah/backend/server.js
+++ childah/backend/server.js
@@ -18,4 +18,17 @@ app.post("/salvar", (req, res) => {
     let idade = req.body.idade;
 
     let sq1 = "INSERT INTO alunos (nome,idade) VALUES (?, ?);"
-})
+
+    conexao.query(sq1, [nome, idade], (erro, resultado) => {
+        if(erro) {
+            console.log(erro);
+        } else {
+            res.send("Aluno salvo com sucesso");
+        }
+    });
+});
+
+app.listen(3000, ()=> {
+    console.log("Servidor rodando na porta 3000");
+});
+
